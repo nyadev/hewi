c2a66de78ed8c42c827c91a1c91f86f4314dce43
Omar616 <30815256+Omar616@users.noreply.github.com>
2018-03-11 11:55:06 -0600

    Fui yo: Batman

--- src/app/users/therapist/diary/sessions/session.component.ts
+++ src/app/users/therapist/diary/sessions/session.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Session } from './session.model';
+import {sessionService} from './sesion.service';
 
-const session = new Session(1, 'CURP', new Date(), 1, 'Observaciones');
+//const session = new Session(1, 'CURP', new Date(), 1, 'Observaciones');
+
 
 @Component({
   selector: 'app-therapist-session',
   templateUrl: './session.component.html',
-  styleUrls: ['./session.component.css']
+  styleUrls: ['./session.component.css'],
+  providers: [sessionService]
 })
-export class SessionComponent {
-  sessions = new Array(5).fill(session);
+export class SessionComponent implements OnInit {
+  constructor(private SessionService : sessionService){
+
+  }
+
+//  sessions = new Array(5).fill(session);
+    sessions : sesion[]:
+    loading : true;
+
+   ngOnInit(){
+     this.SessionService
+        .getSessions()
+        .then((sessions: sessions[])=>{
+          this.sessions = sessions;
+        });
+   }
 }
