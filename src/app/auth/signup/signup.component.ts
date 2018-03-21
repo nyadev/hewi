113c9d51a6f5f465e8cbbaa8308149477cdd7bc4
nyadev <nyadev.ipn@gmail.com>
2018-03-21 17:31:04 -0600

    21-03-18

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,7 +14,33 @@ export class SignupComponent implements OnInit {
      this.signupForm = new FormGroup({
        pNameP: new FormControl(null, [
          Validators.required
-       ])
+       ]),
+       mNameP: new FormControl(null, []),
+       nameP: new FormControl(null, []),
+       username: new FormControl(null, []),
+       password: new FormControl(null, []),
+       genderP: new FormControl(null, []),
+       datebornP: new FormControl(null, []),
+       ageP: new FormControl(null, []),
+       lateralidadP: new FormControl(null, []),
+       escolaridadP: new FormControl(null, []),
+       diagnostico: new FormControl(null, []),
+       estudios: new FormControl(null, []),
+       pNameT: new FormControl(null, []),
+       mNameT: new FormControl(null, []),
+       nameT: new FormControl(null, []),
+       address: new FormControl(null, []),
+       extnumber: new FormControl(null, []),
+       intnumber: new FormControl(null, []),
+       colonia: new FormControl(null, []),
+       delegacion: new FormControl(null, []),
+       postalcode: new FormControl(null, []),
+       state: new FormControl(null, []),
+       escolaridadT: new FormControl(null, []),
+       ageT: new FormControl(null, []),
+       phone: new FormControl(null, []),
+       cellphone: new FormControl(null, []),
+       email: new FormControl(null, []),
      });
    }
 
