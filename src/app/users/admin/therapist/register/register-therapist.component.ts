d6736bba24a7fe9ba8c2a22072cd715abbd99302
Omar616 <30815256+Omar616@users.noreply.github.com>
2018-03-27 19:07:30 -0600

    pruebalo_backend_frontend_signup

--- src/app/users/admin/therapist/register/register-therapist.component.ts
+++ src/app/users/admin/therapist/register/register-therapist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { AuthService } from '../../../../auth/auth.service';
+import { User } from '../../../../auth/user.model';
 @Component({
   selector: 'app-register-therapist',
   templateUrl: './register-therapist.component.html',
@@ -10,6 +11,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterTherapistComponent  implements OnInit {
   registertherapistForm: FormGroup;
 
+  constructor(private AuthService: AuthService){
+
+  }
+
   ngOnInit() {
     this.registertherapistForm = new FormGroup({
       lNameF: new FormControl(null, [
@@ -17,7 +22,9 @@ export class RegisterTherapistComponent  implements OnInit {
       ]),
       lNameM: new FormControl(null, []),
       nome: new FormControl(null, []),
+      curp: new FormControl(null, []),
       phone: new FormControl(null, []),
+      email: new FormControl(null, []),
       cellphone: new FormControl(null, []),
       username: new FormControl(null, []),
       password: new FormControl(null, []),
@@ -31,5 +38,17 @@ export class RegisterTherapistComponent  implements OnInit {
       career: new FormControl(null, []),
       posgrade: new FormControl(null, []),
     });
+  }
+
+  onSubmit(){
+    if(this.registertherapistForm.valid){
+      const { lNameF, lNameM, nome,curp, phone, email, cellphone, username, password, address, extnumber, intnumber, colonia, delegacion, postalcode, state, career, posgrade}  = this.registertherapistForm.value;
+      const user = new User(email, password,username,nome , lNameF, lNameM, curp);
+      this.AuthService.signup(user)
+        .subscribe(
+          this.AuthService.login,
+          err => console.log(err)
+        )
+    }
   }
 }
