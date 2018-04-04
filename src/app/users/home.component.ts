db7b1000495fe285d33cde716784ed05bcd5465d
nyadev <nyadev.ipn@gmail.com>
2018-04-03 19:25:37 -0500

    loginylogout

--- src/app/users/home.component.ts
+++ src/app/users/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    if (this.isLoggedIn()) {
+      this.router.navigateByUrl('/' + this.getUserType());
+    } else {
+      this.router.navigateByUrl('/signin');
+    }
+  }
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  getName() {
+    return this.authService.currentUser.firstName;
+  }
+
+  getUserType() {
+    return this.authService.currentUser.userType + '';
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
