3f46817a09d267500dd055b3847740a848cc9861
nyadev <nyadev.ipn@gmail.com>
2018-04-05 08:39:34 -0500

    Ora sí, borrados

--- src/app/users/therapist/therapist-home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-therapist-home',
-  templateUrl: './therapist-home.component.html',
-  styleUrls: ['./therapist-home.component.css']
-})
-
-export class TherapistHomeComponent { }
