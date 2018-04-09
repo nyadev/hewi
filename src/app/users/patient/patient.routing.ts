de7eae7bae54fbc669f319d24b63f95fc936e6b7
nyadev <nyadev.ipn@gmail.com>
2018-04-09 08:04:14 -0500

    09-04-18

--- src/app/users/patient/patient.routing.ts
+++ src/app/users/patient/patient.routing.ts
@@ -1,20 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD
-import { Act2AtentionComponent } from './activity/atention/act2/act2.component';
-import { Act1vsComponent } from './activity/visiospacial/act1vs/act1_vs.component';
-import { Act2vsComponent } from './activity/visiospacial/act2vs/act2_vs.component';
-
-export const PATIENT_ROUTES: Routes = [
-  { path: 'aat2', component: Act2AtentionComponent},
-  { path: 'avs1', component: Act1vsComponent},
-  { path: 'avs2', component: Act2vsComponent},
-=======
 import { Act2AtComponent } from './activity/atention/act2/act2.component';
 import { Act1VsComponent } from './activity/visiospacial/act1vs/act1_vs.component';
+import { Act2VsComponent } from './activity/visiospacial/act2vs/act2_vs.component';
 
 export const PATIENT_ROUTES: Routes = [
   { path: 'aat2', component: Act2AtComponent},
   { path: 'avs1', component: Act1VsComponent},
->>>>>>> 8104105b276bc3f0cd8c188d373e88a0eabfa476
-];
+  { path: 'avs2', component: Act2VsComponent}
+]
