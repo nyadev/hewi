79529c2fddf6dfcb03a7d5ed6aee7229684a7fb7
nyadev <nyadev.ipn@gmail.com>
2018-04-22 16:10:52 -0500

    asíporelmomento

--- server/index.js
+++ server/index.js
@@ -7,14 +7,6 @@ import { mongoUrl } from './config'
 const  PORT = 3000 || process.env.PORT
 const debug = new Debug('hewi:root')
 
-mongoose.Promise = global.Promise
-
-async function start () {
-  await mongoose.connect(mongoUrl)
-
-  app.listen(PORT, () => {
-    debug(`Server running at port ${PORT}`)
-  })
-}
-
-start()
+app.listen(PORT, () => {
+  debug(`Server running at port ${PORT}`)
+})
